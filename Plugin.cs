84fe04f720c24d22c3f20bf4139edbfd1d1b7f5d
Random Facades <random.facades@gmail.com>
2025-08-22 08:06:07 -0400

    Intial Build

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace HouseRebuild;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource logger;
+
+    private void Awake()
+    {
+        logger = base.Logger;
+        logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
